29693f46f7f6ca80b99b7d8b12c25e76529272d5
wasin.th <wasin.th@tailor-solutions.com>
2025-12-03 16:05:02 +0700

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   // Set global prefix for all routes
   app.setGlobalPrefix('api');
